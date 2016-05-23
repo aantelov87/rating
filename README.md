92970148719c561fb6fc297d62d58dbaa0c7ceed
Antonio Antelo <aantelov87@gmail.com>
2016-05-23 20:59:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+rating
+======
+
+A Symfony project created on May 23, 2016, 5:48 pm.
+# Rating
---
+++ src/RatingBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RatingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('RatingBundle:Default:index.html.twig');
+    }
+}
