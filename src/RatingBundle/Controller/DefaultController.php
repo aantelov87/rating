7d29d2a50891112158a7fadc79d116e44937fa3b
Antonio Antelo <aantelov87@gmail.com>
2016-05-24 17:40:50 +0200

    Add functional test to RatingBundle:DefaultController Add configuration files to run phpunit with test Add tests for widgetAction Add tests for viewRatingAction

--- src/RatingBundle/Controller/DefaultController.php
+++ src/RatingBundle/Controller/DefaultController.php
@@ -29,7 +29,7 @@ class DefaultController extends Controller
                 'user'=>$user
             ])
         );
-        $response->headers->set('Content-Type', 'application/javascript');
+        $response->headers->set('Content-Type', 'application/javascript; charset=UTF-8');
         return $response;
     }
 
--- src/RatingBundle/Tests/Controller/DefaultControllerTest.php
+++ src/RatingBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,17 +1,60 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: aantelov
+ * Date: 5/24/16
+ * Time: 4:40 PM
+ */
 
 namespace RatingBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testWidget()
     {
-        $client = static::createClient();
+        $userWidgetCase = [
+            ['9d19a2f4-21a3-11e6-b374-0242ac110003', [200, 'application/javascript; charset=UTF-8', 'function']],
+            ['9r19r2r4-21a3-11e6-b374-0242ac110003', [404, 'text/html; charset=UTF-8', 'No route']],
+            ['c7b14b9f-21c0-11e6-b374-0242ac110003', [404, 'text/html; charset=UTF-8', 'Resource not found']],
+        ];
 
-        $crawler = $client->request('GET', '/');
+        foreach($userWidgetCase as $widgetCase){
+            $client = static::createClient();
+            $expectedResult = $widgetCase[1];
+            $client->request('GET', "/widget/{$widgetCase[0]}.js");
+            $this->assertSame($expectedResult[0], $client->getResponse()->getStatusCode());
+            $this->assertSame($expectedResult[1], $client->getResponse()->headers->get('Content-Type'));
+            $this->assertTrue( FALSE !== strpos($client->getResponse()->getContent(), $expectedResult[2]));
+        }
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+
+    public function testViewRating()
+    {
+        $userWidgetCase = [
+            ['9d19a2f4-21a3-11e6-b374-0242ac110003', [200, 'text/html; charset=UTF-8', '18%']],
+            ['7145aec7-21c3-11e6-b374-0242ac110003', [200, 'text/html; charset=UTF-8', '0%']],
+            ['9r19r2r4-21a3-11e6-b374-0242ac110003', [404, 'text/html; charset=UTF-8', 'No route']],
+            ['c7b14b9f-21c0-11e6-b374-0242ac110003', [404, 'text/html; charset=UTF-8', 'Resource not found']],
+        ];
+
+        foreach($userWidgetCase as $widgetCase){
+            $client = static::createClient();
+            $expectedResult = $widgetCase[1];
+            $crawler = $client->request('GET', "/user/{$widgetCase[0]}/rating_average");
+            $this->assertSame($expectedResult[0], $client->getResponse()->getStatusCode());
+            $this->assertSame($expectedResult[1], $client->getResponse()->headers->get('Content-Type'));
+            if ($client->getResponse()->isSuccessful()){
+                print $expectedResult[2]."----".$crawler->filter("span.widget-content")->text();
+                $this->assertSame($expectedResult[2], $crawler->filter("span.widget-content")->text());
+            } else {
+                $this->assertTrue( FALSE !== strpos($client->getResponse()->getContent(), $expectedResult[2]));
+            }
+            
+
+        }
+
     }
 }
